33841fb6e6aff4783b323357e261ec1fcc16db75
Dmitry Demidov <dmitry.demidov@gmail.com>
2024-09-06 09:21:21 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("Private NFT","PRNFT")
+        ERC721("TKNFT","TKN")
         Ownable(initialOwner)
     {}
 
